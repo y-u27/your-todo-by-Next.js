77e133d440bf1ce5154a4c94f2193feece636886
Atd27 <y518a480@gmail.com>
2024-07-31 23:48:45 +0900

    supabaseの導入とデータベース連携

---
+++ prisma/migrations/20240731142347_init/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "Post" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "content" TEXT,
+    "status" TEXT NOT NULL,
+
+    CONSTRAINT "Post_pkey" PRIMARY KEY ("id")
+);
