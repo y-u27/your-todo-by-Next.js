82dc140e47916843bcf72659508f928ad0b06427
Atd27 <y518a480@gmail.com>
2024-08-03 23:42:55 +0900

    api修正

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -1,7 +1,33 @@
 import { NextResponse } from "next/server";
 import prisma from "@/app/lib/prismaClient";
 
-export async function GET(req: Request) {
-  const allTodoPost = await prisma.post.findMany();
-  return NextResponse.json(allTodoPost);
+// 型定義
+type TodoRequest = {
+  title: string;
+  content: string;
+  status?: string;
+};
+
+// Todoを作成する関数
+export async function POST(request: Request): Promise<NextResponse> {
+  // リクエストを取得
+  const { title, content, status }: TodoRequest = await request.json();
+
+  //Todo作成
+  const todo = await prisma.post.create({
+    data: {
+      title,
+      content,
+      status,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      success: true,
+      message: "Todoが正常に作成されました",
+      data: todo,
+    },
+    { status: 201 }
+  );
 }
--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,18 +1,6 @@
 import TodoList from "@/components/TodoList";
-import { TodoData } from "../types/types";
-
-async function getTodoAllData() {
-  const response = await fetch("http://localhost:3000/api/todos", {
-    cache: "no-store",
-  });
-
-  const todoAllData: TodoData = await response.json();
-
-  return todoAllData;
-}
 
 export default async function Home() {
-  const todoAllData = await getTodoAllData();
 
   return (
     <>
