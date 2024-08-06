9f050df7fefeea6ea8528c54ad273bd5c58b753e
Atd27 <y518a480@gmail.com>
2024-08-07 01:23:04 +0900

    データ連携するための処理を追加中

--- src/components/TodoEdit.tsx
+++ src/components/TodoEdit.tsx
@@ -1,21 +1,71 @@
 //・TODO編集
+"use client"
 import {
   Box,
   Button,
   Card,
   CardBody,
-  FormControl,
-  FormLabel,
   HStack,
   Input,
-  Textarea,
+  Select,
+  Text,
+  useToast,
 } from "@chakra-ui/react";
+import { useRouter } from "next/navigation";
+import { useRef } from "react";
 
-const TodoEdit = () => {
+// const editTodos = async (
+//   title: string | undefined,
+//   content: string | undefined,
+//   dueDate: number,
+//   status: string | undefined,
+//   id: number
+// ) => {
+//   const res = await fetch(`http://localhost:3000/api/todos/${id}`, {
+//     method: "PATCH",
+//     headers: {
+//       "Content-Type": "application/json",
+//     },
+//     body: JSON.stringify({ title, content, status,dueDate }),
+//   });
+
+//   return res.json();
+// };
+
+const TodoEdit = ({ params }: { params: { id: number } }) => {
+  // const router = useRouter();
+  // const toast = useToast();
+  // const titleRef = useRef<HTMLInputElement | null>(null);
+  // const contentRef = useRef<HTMLInputElement | null>(null);
+  // const statusRef = useRef<HTMLSelectElement | null>(null);
+  // const dueDateRef = useRef<HTMLInputElement | null>(null);
+
+  // const handleSubmit = async (e: React.FormEvent) => {
+  //   e.preventDefault();
+
+  //   toast({
+  //     title: "Todo編集完了",
+  //     description: "Todoが編集されました!",
+  //     status: "success",
+  //     duration: 5000,
+  //     isClosable: true,
+  //   });
+  //   await editTodos(
+  //     titleRef.current?.value,
+  //     contentRef.current?.value,
+  //     dueDate,
+  //     statusRef.current?.value,
+  //     params.id
+  //   );
+
+  //   router.push("/todos");
+  //   router.refresh();
+  // };
+
   return (
     <>
-      <FormControl padding="100px">
-        <FormLabel
+      <Box padding="100px">
+        <Text
           w="50%"
           mx="320px"
           display="flex"
@@ -27,41 +77,46 @@ const TodoEdit = () => {
           mb={5}
         >
           Todo編集ページ
-        </FormLabel>
+        </Text>
         <Card w="50%" mx="320px" shadow="lg">
           <CardBody>
-            <FormControl>
+            <Box>
               <HStack>
-                <FormLabel>Todoタイトル：</FormLabel>
+                <Text>Todoタイトル：</Text>
                 <Input w="70%" h={10} />
               </HStack>
               <br />
               <HStack>
-                <FormLabel>Todo内容：</FormLabel>
+                <Text>Todo内容：</Text>
                 <Input w="70%" h={10} />
               </HStack>
               <br />
               <HStack>
-                <FormLabel>期限：</FormLabel>
-                <Input w="70%" h={10} />
+                <Text>期限：</Text>
+                <Input w="70%" h={10} placeholder='Select Date and Time' size='md' type='datetime-local' />
               </HStack>
               <br />
               <HStack>
-                <FormLabel>ステータス：</FormLabel>
-                <Input w="70%" h={10} />
+                <Text>ステータス：</Text>
+                <Select mx="30px">
+                  <option value="未完了">未完了</option>
+                  <option value="進行中">進行中</option>
+                  <option value="完了">完了</option>
+                </Select>
               </HStack>
-            </FormControl>
+            </Box>
             <Box pt={3} display="flex" justifyContent="center">
               <Button
                 w={130}
                 _hover={{ backgroundColor: "#ffd700", color: "white" }}
+                // onClick={handleSubmit}
               >
                 保存
               </Button>
             </Box>
           </CardBody>
         </Card>
-      </FormControl>
+      </Box>
     </>
   );
 };
