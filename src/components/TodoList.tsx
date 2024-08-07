6cb06ba5299137824cee029422ff7520d57b92ab
Atd27 <y518a480@gmail.com>
2024-08-07 23:24:25 +0900

    ♻️ Todo作成ボタンをクリックした時の動きの追加

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -25,35 +25,41 @@ export default async function TodoList() {
           <Button
             w={700}
             bgColor="#b0c4de"
-            _hover={{ backgroundColor: "#4169e1", color: "white" }}
+            _hover={{
+              backgroundColor: "#4169e1",
+              color: "white",
+            }}
+            _active={{ transform: "scale(0.85)", opacity: 0.1 }}
           >
             Todo作成
           </Button>
         </Link>
       </Box>
-      {data.map((data, id) => (
-        <Card key={data.id} w={700} mx={370} mt={4} mb={4} shadow="lg">
-          <CardBody>
-            <Box>
-              <HStack>
-                <Text>Todo番号：{data.id}</Text>
-              </HStack>
-              <br />
-              <HStack>
-                <Text>Todoタイトル：{data.title}</Text>
-              </HStack>
-              <br />
-              <HStack>
-                <Text>Todo内容：{data.content}</Text>
-              </HStack>
-              <br />
-              <HStack>
-                <Text>Todoステータス：{data.status}</Text>
-              </HStack>
-            </Box>
-          </CardBody>
-        </Card>
-      ))}
+      <Link href="/todos/edit">
+        {data.map((data) => (
+          <Card key={data.id} w={700} mx={370} mt={4} mb={4} shadow="lg">
+            <CardBody>
+              <Box>
+                <HStack>
+                  <Text>Todo番号：{data.id}</Text>
+                </HStack>
+                <br />
+                <HStack>
+                  <Text>Todoタイトル：{data.title}</Text>
+                </HStack>
+                <br />
+                <HStack>
+                  <Text>Todo内容：{data.content}</Text>
+                </HStack>
+                <br />
+                <HStack>
+                  <Text>Todoステータス：{data.status}</Text>
+                </HStack>
+              </Box>
+            </CardBody>
+          </Card>
+        ))}
+      </Link>
     </>
   );
 }
