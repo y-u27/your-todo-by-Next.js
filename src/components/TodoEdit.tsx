b4974ccf93105c4a0d09e6b4f1585d0a1cfed3d4
Atd27 <y518a480@gmail.com>
2024-08-07 23:25:37 +0900

    ✨ useRefなどを使ってのTodo更新機能追加（修正中）

--- src/components/TodoEdit.tsx
+++ src/components/TodoEdit.tsx
@@ -1,5 +1,5 @@
 //・TODO編集
-"use client"
+"use client";
 import {
   Box,
   Button,
@@ -14,53 +14,56 @@ import {
 import { useRouter } from "next/navigation";
 import { useRef } from "react";
 
-// const editTodos = async (
-//   title: string | undefined,
-//   content: string | undefined,
-//   dueDate: number,
-//   status: string | undefined,
-//   id: number
-// ) => {
-//   const res = await fetch(`http://localhost:3000/api/todos/${id}`, {
-//     method: "PATCH",
-//     headers: {
-//       "Content-Type": "application/json",
-//     },
-//     body: JSON.stringify({ title, content, status,dueDate }),
-//   });
+const editTodos = async (
+  title: string | undefined,
+  content: string | undefined,
+  dueDate: number,
+  status: string | undefined,
+) => {
+  const res = await fetch(`http://localhost:3000/api/todos`, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ title, content, status, dueDate }),
+  });
 
-//   return res.json();
-// };
+  return res.json();
+};
 
-const TodoEdit = ({ params }: { params: { id: number } }) => {
-  // const router = useRouter();
-  // const toast = useToast();
-  // const titleRef = useRef<HTMLInputElement | null>(null);
-  // const contentRef = useRef<HTMLInputElement | null>(null);
-  // const statusRef = useRef<HTMLSelectElement | null>(null);
-  // const dueDateRef = useRef<HTMLInputElement | null>(null);
+const TodoEdit = () => {
+  const router = useRouter();
+  const toast = useToast();
+  const titleRef = useRef<HTMLInputElement | null>(null);
+  const contentRef = useRef<HTMLInputElement | null>(null);
+  const statusRef = useRef<HTMLSelectElement | null>(null);
+  const dueDateRef = useRef<HTMLInputElement | null>(null);
 
-  // const handleSubmit = async (e: React.FormEvent) => {
-  //   e.preventDefault();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-  //   toast({
-  //     title: "Todo編集完了",
-  //     description: "Todoが編集されました!",
-  //     status: "success",
-  //     duration: 5000,
-  //     isClosable: true,
-  //   });
-  //   await editTodos(
-  //     titleRef.current?.value,
-  //     contentRef.current?.value,
-  //     dueDate,
-  //     statusRef.current?.value,
-  //     params.id
-  //   );
+    const dueDate = dueDateRef.current
+      ? new Date(dueDateRef.current.value).getTime()
+      : 0;
 
-  //   router.push("/todos");
-  //   router.refresh();
-  // };
+    toast({
+      title: "Todo更新",
+      description: "Todoが更新されました!",
+      status: "success",
+      duration: 5000,
+      isClosable: true,
+    });
+
+    await editTodos(
+      titleRef.current?.value,
+      contentRef.current?.value,
+      dueDate,
+      statusRef.current?.value,
+    );
+
+    router.push("/todos");
+    router.refresh();
+  };
 
   return (
     <>
@@ -83,22 +86,29 @@ const TodoEdit = ({ params }: { params: { id: number } }) => {
             <Box>
               <HStack>
                 <Text>Todoタイトル：</Text>
-                <Input w="70%" h={10} />
+                <Input w="70%" h={10} ref={titleRef} />
               </HStack>
               <br />
               <HStack>
                 <Text>Todo内容：</Text>
-                <Input w="70%" h={10} />
+                <Input w="70%" h={10} ref={contentRef} />
               </HStack>
               <br />
               <HStack>
                 <Text>期限：</Text>
-                <Input w="70%" h={10} placeholder='Select Date and Time' size='md' type='datetime-local' />
+                <Input
+                  w="70%"
+                  h={10}
+                  placeholder="Select Date and Time"
+                  size="md"
+                  type="datetime-local"
+                  ref={dueDateRef}
+                />
               </HStack>
               <br />
               <HStack>
                 <Text>ステータス：</Text>
-                <Select mx="30px">
+                <Select mx="30px" ref={statusRef}>
                   <option value="未完了">未完了</option>
                   <option value="進行中">進行中</option>
                   <option value="完了">完了</option>
@@ -109,9 +119,9 @@ const TodoEdit = ({ params }: { params: { id: number } }) => {
               <Button
                 w={130}
                 _hover={{ backgroundColor: "#ffd700", color: "white" }}
-                // onClick={handleSubmit}
+                onClick={handleSubmit}
               >
-                保存
+                更新
               </Button>
             </Box>
           </CardBody>
