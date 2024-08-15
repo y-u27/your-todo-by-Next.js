cc0d21f4e5484d2f28f87dbf2a850f3597d641af
Atd27 <y518a480@gmail.com>
2024-08-15 23:26:38 +0900

    propsとしてid設定

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -2,7 +2,6 @@ import { Article } from "@/app/types/types";
 import TodoArticle from "@/components/TodoArticle";
 
 const pageId = ({ id }: Article) => {
-  console.log("ID in pageId:", id);
   
   return (
     <>
