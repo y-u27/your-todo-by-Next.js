84ca7d623f9e38593094c7710d463c7001695853
Atd27 <y518a480@gmail.com>
2024-08-19 23:48:23 +0900

    非同期処理移行

--- src/app/todos/page.tsx
+++ src/app/todos/page.tsx
@@ -1,9 +1,20 @@
 import TodoList from "@/components/TodoList";
+import { TodoData } from "../types/types";
+
+async function fetchAllTodos(): Promise<TodoData[]> {
+  const res = await fetch(`http://localhost:3000/api/todos`, {
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+  return data.data;
+}
 
 export default async function Home({ params }: { params: { id: number } }) {
+  const data: TodoData[] = await fetchAllTodos();
   return (
     <>
-      <TodoList id={params.id} />
+      <TodoList id={params.id} todos={data} />
     </>
   );
 }
