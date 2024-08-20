8aeaf5ac06c5174f65050c90c85ad18cd1175a2f
Atd27 <y518a480@gmail.com>
2024-08-21 00:00:39 +0900

    非同期処理移行

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,12 +1,20 @@
+import { TodoData } from "@/app/types/types";
 import TodoArticle from "@/components/TodoArticle";
 
-const pageId = ({ params }: { params: { id: number } }) => {
-  
+async function fetchAllTodos(): Promise<TodoData[]> {
+  const res = await fetch(`http://localhost:3000/api/todos`, {
+    cache: "no-store",
+  });
+
+  const data = await res.json();
+  return data.data;
+}
+
+export default async function pageId({ params }: { params: { id: number } }) {
+  const data: TodoData[] = await fetchAllTodos();
   return (
     <>
-      <TodoArticle id={params.id} />
+      <TodoArticle id={params.id} todos={data} />
     </>
   );
 };
-
-export default pageId;
