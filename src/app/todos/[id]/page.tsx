055d18196be737d3ea0bd16db2a311e3ca3ccb34
Atd27 <y518a480@gmail.com>
2024-08-22 00:45:55 +0900

    🔥 非同期処理部分削除

--- src/app/todos/[id]/page.tsx
+++ src/app/todos/[id]/page.tsx
@@ -1,20 +1,11 @@
-import { TodoData } from "@/app/types/types";
 import TodoArticle from "@/components/TodoArticle";
 
-async function fetchAllTodos(): Promise<TodoData[]> {
-  const res = await fetch(`http://localhost:3000/api/todos`, {
-    cache: "no-store",
-  });
-
-  const data = await res.json();
-  return data.data;
-}
-
-export default async function pageId({ params }: { params: { id: number } }) {
-  const data: TodoData[] = await fetchAllTodos();
+const pageId = ({ params }: { params: { id: number } }) => {
   return (
     <>
-      <TodoArticle id={params.id} todos={data} />
+      <TodoArticle id={params.id} />
     </>
   );
 };
+
+export default pageId;
